cac00a6027dc6fa287b6f68512b8fdf8e891e603
xnought <bertuccd@oregonstate.edu>
2023-12-10 18:45:13 -0800

    feat: first implimentation

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # foldseek-bindings
 
-Simple interface to use `easy-search` from [foldseek](https://github.com/steineggerlab/foldseek). 
+Simple interface to use `easy-search` from [foldseek](https://github.com/steineggerlab/foldseek) from the great people below
+
+> van Kempen, M., Kim, S.S., Tumescheit, C., Mirdita, M., Lee, J., Gilchrist, C.L.M., Söding, J., and Steinegger, M. Fast and accurate protein structure search with Foldseek. Nature Biotechnology, doi:10.1038/s41587-023-01773-0 (2023)
+
+all credit for foldseek goes to them, this is just a python interface. 
 
 The ideal implementation uses pybind and compiles their c++, but I'm too lazy and just called it via exec and read stdout. 
 
@@ -9,3 +13,4 @@ In other words, there is much room for improvement here.
 ## Running
 
 First install the foldseek executable and make sure it's in your path as `foldseek`. 
+
--- foldseek.py
+++ foldseek.py
@@ -1,2 +1,57 @@
+import subprocess
+
+
+def exec(cmd: str | list[str]) -> str:
+    return subprocess.check_output(cmd, shell=True).decode()
+
+
+def parse_output(filepath: str) -> list[list]:
+    with open(filepath, "r") as f:
+        lines = f.readlines()
+
+        parsed_lines = []
+        for line in lines:
+            parsed_line = []
+            for column in line.strip("\n").split("\t"):
+                try:
+                    column = float(column)
+                except ValueError:
+                    pass
+                parsed_line.append(column)
+            parsed_lines.append(parsed_line)
+
+    return parsed_lines
+
+
+def easy_search(
+    query: str,
+    targets: str,
+    out_file=".foldseek_cache/output",
+    temp_dir=".foldseek_cache",
+    print_stdout=False,
+    foldseek_executable="foldseek",
+) -> list[list]:
+    """easy_search just calls foldseek easy-search under the hood
+    TODO: use pybind to call the C++ function instead
+
+    Returns:
+        list[list]: a list of the matches from the search
+    """
+
+    # don't continue unless they actually have foldseek installed
+    if exec(f"which {foldseek_executable}") == "":
+        raise Exception("foldseek not found in PATH")
+
+    # Then call the easy-search
+    cmd = f"{foldseek_executable} easy-search {query} {targets} {out_file} {temp_dir}"
+    stdout = exec(cmd)
+    if print_stdout:
+        print(stdout)
+
+    # parse the text into array of arrays where each column is a data field
+    return parse_output(out_file)
+
+
 if __name__ == "__main__":
-    print("foldseek")
+    output = easy_search("test_examples/A.pdb", "test_examples")
+    print(output)
